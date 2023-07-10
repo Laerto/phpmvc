bde07f700638c66d5fecb3c708b1af7b4d459832
Laerto <dev-console@hotmail.com>
2023-07-10 14:00:26 +0000

    first comit

---
+++ app/core/config.php
@@ -0,0 +1,31 @@
+<?php
+
+
+
+if($_SERVER['SERVER_NAME'] == 'localhost')
+{
+
+    /** Database Config */
+    define('DBNAME', 'mvc');
+    define('DBHOST', 'localhost');
+    define('DBUSER', 'mvc');
+    define('DBPASS', '@test');
+    define('DBDRIVER', 'mysql');
+
+    define('ROOT', 'http://localhost/');
+}else {
+
+    /** Database Config */
+    define('DBNAME', 'mvc');
+    define('DBHOST', 'localhost');
+    define('DBUSER', 'mvc');
+    define('DBPASS', '@test');
+    define('DBDRIVER', 'mysql');
+
+    define('ROOT', 'https://www.example.com');
+}
+
+define('APP_NAME', 'PHP_MVC_Core');
+define('APP_DESC', 'This is a PHP MVC Framework');
+
+define('DEBUG', true);
